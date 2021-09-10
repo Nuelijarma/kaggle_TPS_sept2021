32ac5adbb3cdd5229346b6dbd434408029a409fd
Maya <amelie.barbe@ens-lyon.fr>
2021-09-10 23:25:35 +0200

    XGB classifier, no Nan filling, Nan count as new features.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-# import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
+from xgboost import XGBClassifier
+
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
 
 # Debugging
 from pdb import set_trace as bp
@@ -11,41 +19,50 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-if __name__=="__main__":
+def main():
     logger.debug("Loading training data.")
-    data = pd.read_csv("data/train.csv")
-    # train_id = train_data[["id"]] # Note that we don't need id.
-    x = data.drop(["id", "claim"], axis=1)
+    data = pd.read_csv("data/train.csv", index_col="id")
+    x = data.drop(["claim"], axis=1)
     y = data[["claim"]]
 
     logger.debug("Processing training data.")
-    logger.debug("- Handling NaN values.")
-    x_med = x.median(axis=0)
-    x.fillna(x_med, inplace=True)
+    logger.debug("- Adding NaN count as a feature.")
+    x["na"] = x.isna().sum(axis=1)
+    # logger.debug("- Handling NaN values.")
+    # x_med = x.mean(axis=0)
+    # x.fillna(x_med, inplace=True)
+    logger.debug("- Normalizing features.")
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
 
     logger.debug("Train/test split.")
     train_x, test_x, train_y, test_y = train_test_split(x, y)
 
-    logger.debug("Training Gaussian Naive Bayes classifier.")
-    clf = GaussianNB()
+    logger.debug("Training XGB classifier.")
+    clf = XGBClassifier()
     clf.fit(train_x, train_y.values.ravel())
 
     logger.debug("Testing classifier.")
-    print(f"test accuracy: {clf.score(test_x, test_y)}")
+    print(f"test score: {roc_auc_score(test_y, clf.predict_proba(test_x)[:,1])}")
 
     logger.debug("Loading validation data.")
-    val_data = pd.read_csv("data/test.csv")
-    val_id = val_data[["id"]] # But here we do need id.
-    val_x = val_data.drop(["id"], axis=1)
+    val_x = pd.read_csv("data/test.csv", index_col="id")
+    val_index = val_x.index
 
     logger.debug("Processing validation data.")
-    logger.debug("- Handling NaN values.")
-    val_x.fillna(x_med, inplace=True)
+    logger.debug("- Adding NaN count as a feature.")
+    val_x["na"] = val_x.isna().sum(axis=1)
+    # logger.debug("- Handling NaN values.")
+    # val_x.fillna(x_med, inplace=True)
+    logger.debug("- Normalizing features.")
+    val_x = scaler.transform(val_x)
 
     logger.debug("Predicting test 'claim' value.")
-    val_y = clf.predict(val_x)
+    val_y = clf.predict_proba(val_x)
 
     logger.debug("Writing output")
-    val_y = pd.DataFrame(data=val_y.reshape((-1,1)), columns=["claim"])
-    val_final = val_id.join([val_y])
-    val_final.to_csv("naive_bayes_solution.csv", index=False)
+    val_final = pd.DataFrame(data={"id": val_index.values, "claim": val_y[:,1]})
+    val_final.to_csv("xgb_solution.csv", index=False)
+
+if __name__=="__main__":
+    main()
